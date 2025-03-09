e3c28d7a70fb86a155dc4c867e2ba82123eb0d03
Hrushikesh <sannidhanamhrushikesh@gmail.com>
2025-03-09 15:10:57 +0530

    update profile added

--- backend/src/controllers/auth.conotroller.js
+++ backend/src/controllers/auth.conotroller.js
@@ -65,3 +65,39 @@ export const logout = async (req, res) => {
         })
     }
 }
+export const updateProfile = async function(req,res){
+    try {
+        const {email,name,age,grade,subjects}=req.body;
+        const user=await User.find(email);
+        if(!user){
+            res.status(404).json({
+                message:"User not found!"
+            })
+        }
+        if(user.email!=email){
+            message:"Email cannot be Changed";
+        }
+        if(name){  
+            user.name=name;
+        }
+        if(age){
+            user.age=age;
+        }
+        if(grade){
+            user.grade=grade;
+        }
+        if(subjects){
+            user.subjects=subjects;
+        }
+        await user.save();
+        res.status(200).json({
+            message:"Profile Updated!"
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            message:"Internal Server Error!"
+        })
+    } 
+                 
+}
