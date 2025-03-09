1044fcdd476a3349d61bae519025d368ecc15c86
KGP09 <gunakoppoju@gmail.com>
2025-03-09 18:53:42 +0530

    Saving Local Changes before pulling

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,16 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "../backend/src/routes/auth.routes.js";
-import { connectDB } from "./src/lib/db.js"; 
-import {  generateStudyPlan  } from "./ai-planner.js";
+import { connectDB } from "./src/lib/db.js";
+import { generateStudyPlan } from "./ai-planner.js";
 import { generateQuiz } from "./ai-quiz.js";
 const app = express();
 dotenv.config()
 const PORT = process.env.PORT
 app.use(express.json())
 app.use("/api/auth", authRoutes)
-app.use(express.urlencoded({ extended: true }));app.use(express.json())
-
-app.post("/ai", async (req, res) => {
+app.use(express.urlencoded({ extended: true })); app.use(express.json())
+app.post("/api/ai-planner", async (req, res) => {
   try {
     const { difficulty, deadline, est, type, studyHours, topic } = req.body;
     const response = await generateStudyPlan(
@@ -29,11 +28,13 @@ app.post("/ai", async (req, res) => {
   }
 });
 
-app.post("/quiz", async (req, res) => {
+app.post("/api/init-quiz", async (req, res) => {
   try {
     const { subjects, age } = req.body;
-    const response = await generateQuiz(subjects, age);
+    const response = (await generateQuiz(subjects, age))
     res.status(200).json(response);
+    console.log(JSON.parse(response));
+
   } catch (err) {
     res.json(err);
   }
